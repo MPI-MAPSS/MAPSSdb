7c220099d98c24ddab3d5ea84d05c957fbfdec94
Jacob Walls <jwalls@fargeo.com>
2023-11-03 09:44:20 -0400

    Update project to 7.5

--- mapss/urls.py
+++ mapss/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.urls import re_path
 
 urlpatterns = [
-    url(r'^', include('arches.urls')),
+    re_path(r'^', include('arches.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.SHOW_LANGUAGE_SWITCH is True:
