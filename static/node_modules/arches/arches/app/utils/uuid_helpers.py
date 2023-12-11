64173ed396f9795a270c8769116f39a24600b26c
Cyrus Hiatt <chiatt@fargeo.com>
2023-12-11 09:08:42 -0800

    remove static files from repo

--- static/node_modules/arches/arches/app/utils/uuid_helpers.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-# Move to util function
-def uuid_get_or_create(id):
-    try:
-        uuid.UUID(id)
-        return uuid.UUID(id), False
-    except (ValueError, TypeError):
-        return uuid.uuid4(), True
